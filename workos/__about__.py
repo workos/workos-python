b860ede4ac3ba0b0014f1cdcd52f783359ab67dd
Henry Chan <henry@workos.com>
2020-01-02 16:44:56 -0500

    Different way to do version

---
+++ workos/__about__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    '__package_name__', '__package_url__', '__version__', '__author__',
+    '__author_email__', '__description__', '__license__',
+]
+
+__package_name__ = 'workos'
+
+__package_url__ = 'https://github.com/workos-inc/workos-python'
+
+__version__ = '0.0.0a1'
+
+__author__ = 'WorkOS'
+
+__author_email__ = 'team@workos.com'
+
+__description__ = 'WorkOS Python Client'
+
+__license__ = 'MIT'

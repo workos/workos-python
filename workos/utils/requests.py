0b6756de28f63121b5123ad6a3ba99cd00a4023d
Henry Chan <henry@workos.com>
2020-01-02 14:50:25 -0500

    Extract request data for exceptions

--- workos/utils/requests.py
+++ workos/utils/requests.py
@@ -25,15 +25,17 @@ class RequestHelper(object):
         url = self.generate_api_url(path)
         response = getattr(requests, method)(url, params=params)
 
-        status_code = request.status_code
+        status_code = response.status_code
         if status_code >= 400 and status_code < 500:
             if status_code == 401:
-                raise AuthorizationException()
+                raise AuthorizationException(response)
             elif status_code == 403:
-                raise AuthenticationException()
-            raise BadRequestException()
+                raise AuthenticationException(response)
+            raise BadRequestException(response)
         elif status_code >= 500 and status_code < 600:
-            raise ServerException()
+            raise ServerException(response)
 
-        
-        return response.json()
+        try:
+            return response.json()
+        except ValueError:
+            raise ServerException(response)

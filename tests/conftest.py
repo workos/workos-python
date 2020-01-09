52d5a610349d9a1c0b2d108f7aae578edfff1c57
Henry Chan <henry@workos.com>
2020-01-08 21:12:58 -0500

    Refactor mocking response

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
 import pytest
+import requests
 
 import workos
+
+class MockResponse(object):
+    def __init__(self, response_dict, status_code):
+        self.response_dict = response_dict
+        self.status_code = status_code
+
+    def json(self):
+        return self.response_dict
 
 @pytest.fixture
 def set_api_key(monkeypatch):
@@ -13,3 +22,13 @@ def set_project_id(monkeypatch):
 @pytest.fixture
 def set_api_key_and_project_id(set_api_key, set_project_id):
     pass
+
+@pytest.fixture
+def mock_request_method(monkeypatch):
+    def _mock_request_method(method, response_dict, status_code):
+        def mock_response(*args, **kwargs):
+            return MockResponse(response_dict, status_code)
+
+        monkeypatch.setattr(requests, method, mock_response)
+
+    return _mock_request_method

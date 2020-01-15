c54373133ba3c6709a5b55aac3f59ea5575c980f
Henry Chan <henry@workos.com>
2020-01-15 15:12:47 -0500

    Add README as long description so we have something on PyPi

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 
 base_dir = os.path.dirname(__file__)
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 about = {}
-with open(os.path.join(base_dir, "workos", "__about__.py")) as f:
+with open(os.path.join(base_dir, 'workos', '__about__.py')) as f:
     exec(f.read(), about)
 
 setup(
@@ -14,6 +17,8 @@ setup(
     author_email=about['__author_email__'],
     url=about['__package_url__'],
     description=about['__description__'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude=['tests*', ]),
     zip_safe=False,
     license=about['__license__'],

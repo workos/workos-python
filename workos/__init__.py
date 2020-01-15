4e7014a5d529df46567ddc55aeb1e72576d2c11a
Henry Chan <henry@workos.com>
2020-01-15 13:33:40 -0500

    PR Feedback

--- workos/__init__.py
+++ workos/__init__.py
@@ -1,8 +1,6 @@
 from workos.__about__ import __version__
 from workos.client import client
 
-BASE_API_URL = 'https://api.workos.com/'
-
 api_key = None
 project_id = None
-base_api_url = BASE_API_URL
+base_api_url = 'https://api.workos.com/'

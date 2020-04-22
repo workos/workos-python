70d5263f85c6e642557c84b90659b8cbbd882019
Henry Chan <henry@workos.com>
2020-04-21 23:22:05 -0400

    Grab config from env by default

--- workos/__init__.py
+++ workos/__init__.py
@@ -1,6 +1,8 @@
+import os
+
 from workos.__about__ import __version__
 from workos.client import client
 
-api_key = None
-project_id = None
+api_key = os.getenv("WORKOS_API_KEY")
+project_id = os.getenv("WORKOS_PROJECT_ID")
 base_api_url = "https://api.workos.com/"

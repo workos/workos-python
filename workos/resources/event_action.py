370ce81199a87b505d8fea84b679bab90e19e7bc
Henry Chan <henry@workos.com>
2020-07-21 16:44:35 -0400

    Support Audit Trail get endpoint

---
+++ workos/resources/event_action.py
@@ -0,0 +1,15 @@
+from workos.resources.base import WorkOSBaseResource
+
+
+class WorkOSEventAction(WorkOSBaseResource):
+    """Representation of an Event Action as returned by WorkOS through the Audit Trail feature.
+    
+    Attributes:
+        OBJECT_FIELDS (list): List of fields a WorkOSEventAction is comprised of.
+    """
+
+    OBJECT_FIELDS = [
+        "id",
+        "name",
+        "project_id",
+    ]

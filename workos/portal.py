d033e2875acb4e00445e358d12153405589cca74
Willman Duffy <willman@workos.com>
2020-09-14 10:54:15 -0400

    Add create_organization function to Portal module
    * Adds the ability to create an organization via the SDK for use
      with the Admin Portal.

--- workos/portal.py
+++ workos/portal.py
@@ -1,7 +1,8 @@
 import workos
-from workos.utils.request import RequestHelper, REQUEST_METHOD_GET
+from workos.utils.request import RequestHelper, REQUEST_METHOD_GET, REQUEST_METHOD_POST
 from workos.utils.validation import PORTAL_MODULE, validate_settings
 
+ORGANIZATIONS_PATH = "organizations"
 RESPONSE_LIMIT = 10
 
 
@@ -15,6 +16,24 @@ class Portal(object):
         if not getattr(self, "_request_helper", None):
             self._request_helper = RequestHelper()
         return self._request_helper
+
+    def create_organization(self, organization):
+        """Create an organization
+
+        Args:
+            organization (dict) - An organization object
+                organization[domains] (list) - List of domains that belong to the organization
+                organization[name] (str) - A unique, descriptive name for the organization
+
+        Returns:
+            dict: Created Organization response from WorkOS.
+        """
+        return self.request_helper.request(
+            ORGANIZATIONS_PATH,
+            method=REQUEST_METHOD_POST,
+            params=organization,
+            token=workos.api_key,
+        )
 
     def list_organizations(
         self, domains=None, limit=RESPONSE_LIMIT, before=None, after=None
@@ -37,7 +56,7 @@ class Portal(object):
             "after": after,
         }
         return self.request_helper.request(
-            "organizations",
+            ORGANIZATIONS_PATH,
             method=REQUEST_METHOD_GET,
             params=params,
             token=workos.api_key,

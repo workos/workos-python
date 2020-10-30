e72859efa65a6f3e09cc98a73af6af5c92bd9b7f
Taylor Hodge <j.taylor.hodge@gmail.com>
2020-10-30 18:26:14 -0400

    Update Available Connection Types (#46)

--- workos/utils/connection_types.py
+++ workos/utils/connection_types.py
@@ -4,5 +4,13 @@ from enum import Enum
 class ConnectionType(Enum):
     ADFSSAML = "ADFSSAML"
     AzureSAML = "AzureSAML"
+    GenericOIDC = "GenericOIDC"
+    GenericSAML = "GenericSAML"
     GoogleOAuth = "GoogleOAuth"
+    GoogleSAML = "GoogleSAML"
+    MagicLink = "MagicLink"
     OktaSAML = "OktaSAML"
+    OneLoginSAML = "OneLoginSAML"
+    PingFederateSAML = "PingFederateSAML"
+    PingOneSAML = "PingOneSAML"
+    VMwareSAML = "VMwareSAML"

d2b9d8687198ab0d9cc54ca181c070fe7aa28dcb
Lo Ferris <lo.ferris@workos.com>
2020-12-23 16:08:37 -0800

    removed project_id environment variable from __init__.py

--- workos/__init__.py
+++ workos/__init__.py
@@ -4,5 +4,4 @@ from workos.__about__ import __version__
 from workos.client import client
 
 api_key = os.getenv("WORKOS_API_KEY")
-project_id = os.getenv("WORKOS_PROJECT_ID")
 base_api_url = "https://api.workos.com/"

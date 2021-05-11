3fe97a78f9e73ebf3dca45c0292704885a7d91b0
erin@workos.com <erin@workos.com>
2021-05-11 11:12:31 -0700

    Change HTTP response code for test_delete_connection

--- tests/test_sso.py
+++ tests/test_sso.py
@@ -293,7 +293,7 @@ class TestSSO(object):
 
     def test_delete_connection(self, setup_with_client_id, mock_request_method):
         mock_response = Response()
-        mock_response.status_code = 200
-        mock_request_method("delete", mock_response, 200)
+        mock_response.status_code = 202
+        mock_request_method("delete", mock_response, 202)
         response = self.sso.delete_connection(connection="connection_id")
-        assert response.status_code == 200
+        assert response.status_code == 202

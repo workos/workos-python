603c400806ef99ebe64c0e2b51f576673462f8f8
Marshall Bowers <marshall@workos.com>
2021-05-14 23:54:28 -0400

    Change development status classifier to "Production/Stable" (#75)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=["requests>=2.22.0"],
     extras_require={"dev": dev_requirements, ":python_version<'3.4'": ["enum34"],},
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",

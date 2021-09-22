7bfa8063e94f88f6fc87cb12be4def229f4bac9b
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2021-09-22 17:09:54 -0600

    logic in place within webhooks.py

--- tests/test_webhooks.py
+++ tests/test_webhooks.py
@@ -10,16 +10,29 @@ from workos.utils.request import RESPONSE_TYPE_CODE
 
 class TestWebhooks(object):
     @pytest.fixture(autouse=True)
-    def setup(self, set_api_key):
+    def setup(self, set_api_key_and_client_id):
         self.webhooks = Webhooks()
 
     @pytest.fixture
-    def mock_method_name(self):
+    def mock_event_body(self):
         return '{"data":{"id":"directory_user_01FAEAJCR3ZBZ30D8BD1924TVG","state":"active","emails":[{"type":"work","value":"blair@foo-corp.com","primary":true}],"idp_id":"00u1e8mutl6wlH3lL4x7","object":"directory_user","username":"blair@foo-corp.com","last_name":"Lunceford","first_name":"Blair","directory_id":"directory_01F9M7F68PZP8QXP8G7X5QRHS7","raw_attributes":{"name":{"givenName":"Blair","familyName":"Lunceford","middleName":"Elizabeth","honorificPrefix":"Ms."},"title":"Developer Success Engineer","active":true,"emails":[{"type":"work","value":"blair@foo-corp.com","primary":true}],"groups":[],"locale":"en-US","schemas":["urn:ietf:params:scim:schemas:core:2.0:User","urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"],"userName":"blair@foo-corp.com","addresses":[{"region":"CO","primary":true,"locality":"Steamboat Springs","postalCode":"80487"}],"externalId":"00u1e8mutl6wlH3lL4x7","displayName":"Blair Lunceford","urn:ietf:params:scim:schemas:extension:enterprise:2.0:User":{"manager":{"value":"2","displayName":"Kathleen Chung"},"division":"Engineering","department":"Customer Success"}}},"event":"dsync.user.created"}'
     
-    def test_print_method_name(self, mock_method_name, mock_request_method):
-        mock_request_method("get", mock_method_name, 200)
+    def mock_header(self):
+        return None
+    
+    
+    def test_print_method_name(self, mock_request_method):
+        # mock_request_method("get", mock_method_name, 200)
 
-        name = self.webhooks.print_method_name
+        result = self.webhooks.verify_event(TestWebhooks.mock_event_body, None)
 
-        assert name == mock_method_name
+        assert result == "Unable to extract timestamp and signature hash from header"
+
+
+    # "Unable to extract timestamp and signature hash from header"
+
+
+    # "Timestamp outside the tolerance zone"
+
+
+    # "Signature hash does not match the expected signature hash for payload"
--- workos/webhooks.py
+++ workos/webhooks.py
@@ -1,27 +1,78 @@
-import workos
-from workos.utils.request import (
-    RequestHelper,
-    REQUEST_METHOD_DELETE,
-    REQUEST_METHOD_GET,
-    REQUEST_METHOD_POST,
-)
+from workos.utils.request import RequestHelper
 from workos.utils.validation import WEBHOOKS_MODULE, validate_settings
-
+import hmac
+import json
+import time
+from collections import OrderedDict
+import hashlib
 
 class Webhooks(object):
     """Offers methods through the WorkOS Webhooks service."""
+    DEFAULT_TOLERANCE = 180
 
     @validate_settings(WEBHOOKS_MODULE)
     def __init__(self):
         pass
 
-    @property
-    def request_helper(self):
-        if not getattr(self, "_request_helper", None):
-            self._request_helper = RequestHelper()
-        return self._request_helper
+    @staticmethod
+    def verify_event(
+        payload, sig_header, secret, tolerance=DEFAULT_TOLERANCE
+    ):
 
-    @property
-    def print_method_name(self):
+        WebhookSignature.verify_header(payload, sig_header, secret, tolerance)
+        event = json.loads(payload, object_pairs_hook=OrderedDict) 
+        return event
+
+
+class WebhookSignature(object):
+    @staticmethod 
+    def constant_time_compare(val1, val2):
+        if len(val1) != len(val2):
+            return False
+        result = 0
+        for x, y in zip(val1, val2):
+            result |= ord(x) ^ ord(y)
+        return result == 0
+
+    @staticmethod
+    def verify_header(event_body, event_signature, secret, tolerance=None):
+        try:
+            # Define variables parsed from the event body
+            issued_timestamp, signature_hash = event_signature.split(', ')
+            issued_timestamp = issued_timestamp[2:]
+            signature_hash = signature_hash[3:]
+
+            # Define time related variables
+            MAX_SECONDS_SINCE_ISSUED = tolerance
+            current_time = time.time()
+            timestamp_in_seconds = int(issued_timestamp) / 1000
+            seconds_since_issued = current_time - timestamp_in_seconds
+
+        except Exception:
+            raise ValueError(
+                "Unable to extract timestamp and signature hash from header",
+                event_body,
+                event_signature,
+            )
+
+        # Check that the webhook timestamp is within the acceptable range
+        if seconds_since_issued > MAX_SECONDS_SINCE_ISSUED:
+            raise ValueError(
+                "Timestamp outside the tolerance zone"
+            )
+
+        #Set expected signature value based on env var secret
+        unhashed_string = f'{issued_timestamp}.{event_body.decode("utf-8")}'
+        expected_signature = hmac.new(
+            secret.encode('utf-8'),
+            unhashed_string.encode('utf-8'),
+            digestmod=hashlib.sha256
+        ).hexdigest()
         
-        return '{"data":{"id":"directory_user_01FAEAJCR3ZBZ30D8BD1924TVG","state":"active","emails":[{"type":"work","value":"blair@foo-corp.com","primary":true}],"idp_id":"00u1e8mutl6wlH3lL4x7","object":"directory_user","username":"blair@foo-corp.com","last_name":"Lunceford","first_name":"Blair","directory_id":"directory_01F9M7F68PZP8QXP8G7X5QRHS7","raw_attributes":{"name":{"givenName":"Blair","familyName":"Lunceford","middleName":"Elizabeth","honorificPrefix":"Ms."},"title":"Developer Success Engineer","active":true,"emails":[{"type":"work","value":"blair@foo-corp.com","primary":true}],"groups":[],"locale":"en-US","schemas":["urn:ietf:params:scim:schemas:core:2.0:User","urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"],"userName":"blair@foo-corp.com","addresses":[{"region":"CO","primary":true,"locality":"Steamboat Springs","postalCode":"80487"}],"externalId":"00u1e8mutl6wlH3lL4x7","displayName":"Blair Lunceford","urn:ietf:params:scim:schemas:extension:enterprise:2.0:User":{"manager":{"value":"2","displayName":"Kathleen Chung"},"division":"Engineering","department":"Customer Success"}}},"event":"dsync.user.created"}'
+        # Use a secure constant time comparison function to ensure the signature hash matches the expected signature value
+        #check time comparison with sig hash from the webhook against the expected sig
+        if WebhookSignature.constant_time_compare(signature_hash, expected_signature):
+            return True
+        else: raise ValueError(
+                "Signature hash does not match the expected signature hash for payload"
+            )

82c9974828be149d5b440ffc51aa2a32dfe3125f
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2021-09-23 17:24:42 -0600

    updated formatting via linter

--- workos/webhooks.py
+++ workos/webhooks.py
@@ -9,6 +9,7 @@ import hashlib
 
 class Webhooks(object):
     """Offers methods through the WorkOS Webhooks service."""
+
     @validate_settings(WEBHOOKS_MODULE)
     def __init__(self):
         pass
@@ -18,19 +19,16 @@ class Webhooks(object):
         if not getattr(self, "_request_helper", None):
             self._request_helper = RequestHelper()
         return self._request_helper
+
     DEFAULT_TOLERANCE = 180
 
     @staticmethod
-    def verify_event(
-        payload, sig_header, secret, tolerance=DEFAULT_TOLERANCE
-    ):
+    def verify_event(payload, sig_header, secret, tolerance=DEFAULT_TOLERANCE):
 
         if payload is None:
-            raise ValueError(
-                "Payload body is missing and is a required parameter")
+            raise ValueError("Payload body is missing and is a required parameter")
         elif sig_header is None:
-            raise ValueError(
-                "Payload signature missing and is a required parameter")
+            raise ValueError("Payload signature missing and is a required parameter")
         elif secret is None:
             raise ValueError("Secret is missing and is a required parameter")
 
@@ -54,7 +52,8 @@ class WebhookSignature(object):
     def constant_time_compare(val1, val2):
         if len(val1) != len(val2):
             raise ValueError(
-                "Signature hash does not match the expected signature hash for payload")
+                "Signature hash does not match the expected signature hash for payload"
+            )
 
         result = 0
         for x, y in zip(val1, val2):
@@ -64,15 +63,13 @@ class WebhookSignature(object):
     @staticmethod
     def check_timstamp_range(time, max_range):
         if time > max_range:
-            raise ValueError(
-                "Timestamp outside the tolerance zone"
-            )
+            raise ValueError("Timestamp outside the tolerance zone")
 
     @staticmethod
     def verify_header(event_body, event_signature, secret, tolerance=None):
         try:
             # Verify and define variables parsed from the event body
-            issued_timestamp, signature_hash = event_signature.split(', ')
+            issued_timestamp, signature_hash = event_signature.split(", ")
         except BaseException:
             raise ValueError(
                 "Unable to extract timestamp and signature hash from header",
@@ -89,17 +86,17 @@ class WebhookSignature(object):
 
         # Check that the webhook timestamp is within the acceptable range
         WebhookSignature.check_timstamp_range(
-            seconds_since_issued, max_seconds_since_issued)
+            seconds_since_issued, max_seconds_since_issued
+        )
 
         # Set expected signature value based on env var secret
         unhashed_string = f"{issued_timestamp}.{event_body}"
         expected_signature = hmac.new(
-            secret.encode('utf-8'),
-            unhashed_string.encode('utf-8'),
-            digestmod=hashlib.sha256
+            secret.encode("utf-8"),
+            unhashed_string.encode("utf-8"),
+            digestmod=hashlib.sha256,
         ).hexdigest()
 
         # Use constant time comparison function to ensure the sig hash matches
         # the expected sig value
-        WebhookSignature.constant_time_compare(
-            signature_hash, expected_signature)
+        WebhookSignature.constant_time_compare(signature_hash, expected_signature)

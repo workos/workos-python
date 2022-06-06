77de42ee1602fdfc97c7903bd8a6a7459c337794
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2022-06-06 13:45:36 -0600

    Add class defining WorkOSDirectoryGroup dict and update connection type Enum list

--- workos/resources/directory_sync.py
+++ workos/resources/directory_sync.py
@@ -2,10 +2,10 @@ from workos.resources.base import WorkOSBaseResource
 
 
 class WorkOSDirectoryGroup(WorkOSBaseResource):
-    """Representation of a User Profile as returned by WorkOS through the SSO feature.
+    """Representation of a Directory Group as returned by WorkOS through the Directory Sync feature.
 
     Attributes:
-        OBJECT_FIELDS (list): List of fields a WorkOSProfile is comprised of.
+        OBJECT_FIELDS (list): List of fields a WorkOSDirectoryGroup is comprised of.
     """
 
     OBJECT_FIELDS = [
@@ -17,3 +17,12 @@ class WorkOSDirectoryGroup(WorkOSBaseResource):
         "updated_at",
         "raw_attributes",
     ]
+
+    @classmethod
+    def construct_from_response(cls, response):
+        return super(WorkOSDirectoryGroup, cls).construct_from_response(response)
+
+    def to_dict(self):
+        directory_group = super(WorkOSDirectoryGroup, self).to_dict()
+
+        return directory_group

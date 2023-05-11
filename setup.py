d8965fea2b1c83d0d1a604ba8265fa698f05eea8
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2023-05-11 14:34:22 -0600

    Pin dependency versions for requests and requests-toolbelt

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ if sys.version_info.major == 3:
         [
             "black==22.3.0",
             "twine==3.1.1",
-            "requests==2.30.0",
+            "requests==2.29.0",
+            "requests-toolbelt==0.9.0"
             "urllib3==2.0.2",
         ]
     )

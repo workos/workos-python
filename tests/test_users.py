6dad983baa55fcdc2e24101372c244c7191d907a
Jônatas Santos <jonatascastro12@gmail.com>
2023-08-21 17:31:13 -0300

    Fix method

--- tests/test_users.py
+++ tests/test_users.py
@@ -227,6 +227,8 @@ class TestUsers(object):
         self, capture_and_mock_request, mock_auth_response
     ):
         code = "test_auth"
+        email = "marcelina@foo-corp.com"
+        password = "test123"
         expires_in = 3600
         user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
         ip_address = "192.0.0.1"
@@ -235,6 +237,8 @@ class TestUsers(object):
 
         response = self.users.authenticate_with_password(
             code=code,
+            email=email,
+            password=password,
             expires_in=expires_in,
             user_agent=user_agent,
             ip_address=ip_address,
@@ -244,9 +248,11 @@ class TestUsers(object):
         assert response["user"]["id"] == "user_01H7ZGXFP5C6BBQY6Z7277ZCT0"
         assert response["session"]["id"] == "session_01E4ZCR3C56J083X43JQXF3JK5"
         assert request["json"]["code"] == code
+        assert request["json"]["email"] == email
+        assert request["json"]["password"] == password
         assert request["json"]["user_agent"] == user_agent
         assert request["json"]["expires_in"] == expires_in
         assert request["json"]["ip_address"] == ip_address
         assert request["json"]["client_id"] == "client_b27needthisforssotemxo"
         assert request["json"]["client_secret"] == "sk_abdsomecharactersm284"
-        assert request["json"]["grant_type"] == "authorization_code"
+        assert request["json"]["grant_type"] == "password"

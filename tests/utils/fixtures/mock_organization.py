f051eff48a05aee4830b2f93bcbc5e3e4d466ceb
Rob Frampton <rob@workos.com>
2023-11-10 11:48:12 -0500

    Remove domains and allowProfilesOutsideOrganization from Organization entity

--- tests/utils/fixtures/mock_organization.py
+++ tests/utils/fixtures/mock_organization.py
@@ -7,17 +7,13 @@ class MockOrganization(WorkOSBaseResource):
         self.id = id
         self.object = "organization"
         self.name = "Foo Corporation"
-        self.allow_profiles_outside_organization = False
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        self.domains = ["domain1.com"]
 
     OBJECT_FIELDS = [
         "id",
         "object",
         "name",
-        "allow_profiles_outside_organization",
         "created_at",
         "updated_at",
-        "domains",
     ]

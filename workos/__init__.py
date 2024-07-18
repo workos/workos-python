46c9109347899c0063fe06da91348137555d5106
mattgd <matt.dzwonczyk@workos.com>
2024-07-18 12:50:10 -0400

    Update default base API URL.

--- workos/__init__.py
+++ workos/__init__.py
@@ -6,7 +6,7 @@ from workos.async_client import AsyncClient
 
 api_key = os.getenv("WORKOS_API_KEY")
 client_id = os.getenv("WORKOS_CLIENT_ID")
-base_api_url = os.getenv("WORKOS_BASE_URL")
+base_api_url = os.getenv("WORKOS_BASE_URL", "https://api.workos.com/")
 request_timeout = int(os.getenv("WORKOS_REQUEST_TIMEOUT", "25"))
 
 

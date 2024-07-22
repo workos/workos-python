6fd2bfc51bbb17735b155f0164784a7f9aa8d64e
Giovanni Carvelli <gio@workos.com>
2024-07-22 13:44:48 -0400

    Add AppleOAuth and GitHubOAuth constants (#277)
    * Add AppleOAuth (and missing GithubOAuth) constants
    
    * Hub

--- workos/utils/um_provider_types.py
+++ workos/utils/um_provider_types.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class UserManagementProviderType(Enum):
     AuthKit = "authkit"
+    AppleOAuth = "AppleOAuth"
     GitHubOAuth = "GitHubOAuth"
     GoogleOAuth = "GoogleOAuth"
     MicrosoftOAuth = "MicrosoftOAuth"

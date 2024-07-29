9e3e55e5b7980a4bdbf3237a4282fbf9a43c4c3d
Peter Arzhintar <pantera@workos.com>
2024-07-29 16:27:15 -0700

    Typo list -> List

--- workos/types/organizations/organization_common.py
+++ workos/types/organizations/organization_common.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, List
 from workos.resources.workos_model import WorkOSModel
 from workos.types.organizations.organization_domain import OrganizationDomain
 
@@ -7,6 +7,6 @@ class OrganizationCommon(WorkOSModel):
     id: str
     object: Literal["organization"]
     name: str
-    domains: list[OrganizationDomain]
+    domains: List[OrganizationDomain]
     created_at: str
     updated_at: str

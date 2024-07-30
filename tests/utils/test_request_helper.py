28ed10aa24d4a5e1872ffd99387d342c5736dab0
mattgd <matt.dzwonczyk@workos.com>
2024-07-30 13:50:37 -0400

    Add additional test.

--- tests/utils/test_request_helper.py
+++ tests/utils/test_request_helper.py
@@ -7,3 +7,7 @@ class TestRequestHelper:
         assert RequestHelper.build_parameterized_url("a/b/c") == "a/b/c"
         assert RequestHelper.build_parameterized_url("a/{b}/c", b="b") == "a/b/c"
         assert RequestHelper.build_parameterized_url("a/{b}/c", b="test") == "a/test/c"
+        assert (
+            RequestHelper.build_parameterized_url("a/{b}/c", b="i/am/being/sneaky")
+            == "a/i/am/being/sneaky/c"
+        )

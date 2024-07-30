8988bd99efbcf56ddd8648881030ba59a3cff016
Peter Arzhintar <pantera@workos.com>
2024-07-30 09:31:49 -0700

    Remove unused connection status

--- workos/types/events/connection_payload_with_legacy_fields.py
+++ workos/types/events/connection_payload_with_legacy_fields.py
@@ -1,8 +1,6 @@
 from typing import Literal
 from workos.resources.sso import ConnectionWithDomains
 
-ConnectionStatus = Literal["linked", "unlinked"]
-
 
 class ConnectionPayloadWithLegacyFields(ConnectionWithDomains):
     external_key: str

c22a5360f1af10cbd25fc8792cc667be4dfd3350
Sherry Ali <sherry@workos.com>
2024-08-15 14:30:35 -0400

    Add certificate renewal intent

--- workos/types/portal/portal_link_intent.py
+++ workos/types/portal/portal_link_intent.py
@@ -1,4 +1,4 @@
 from typing import Literal
 
 
-PortalLinkIntent = Literal["audit_logs", "dsync", "log_streams", "sso"]
+PortalLinkIntent = Literal["audit_logs", "certificate_renewal", "dsync", "log_streams", "sso"]

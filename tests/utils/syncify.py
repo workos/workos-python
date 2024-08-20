994f04af2048a2ec575cd413065423c4a3486f30
mattgd <matt.dzwonczyk@workos.com>
2024-08-20 14:56:20 -0400

    Use genertic for syncify return type.

--- tests/utils/syncify.py
+++ tests/utils/syncify.py
@@ -1,6 +1,8 @@
 import asyncio
 from types import CoroutineType
-from typing import Any
+from typing import Any, TypeVar, Union
+
+ReturnType = TypeVar("ReturnType")
 
 
 def _call(coro: CoroutineType) -> Any:
@@ -12,7 +14,7 @@ def _call(coro: CoroutineType) -> Any:
         return loop.run_until_complete(coro)
 
 
-def run_sync(obj: Any) -> Any:
+def run_sync(obj: Union[CoroutineType, ReturnType]) -> ReturnType:
     if isinstance(obj, CoroutineType):
         return _call(obj)
 

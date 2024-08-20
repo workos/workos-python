daba03b48576fcfebd9e466f4166ba9bf6c3c2bd
mattgd <matt.dzwonczyk@workos.com>
2024-08-20 14:59:14 -0400

    A little better?

--- tests/utils/syncify.py
+++ tests/utils/syncify.py
@@ -1,11 +1,11 @@
 import asyncio
 from types import CoroutineType
-from typing import Any, TypeVar, Union
+from typing import Any, Coroutine, TypeVar, Union
 
 ReturnType = TypeVar("ReturnType")
 
 
-def _call(coro: CoroutineType) -> Any:
+def _call(coro: Coroutine[Any, Any, ReturnType]) -> ReturnType:
     try:
         loop = asyncio.get_running_loop()
     except RuntimeError:

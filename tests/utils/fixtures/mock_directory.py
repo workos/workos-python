f17be5e2640ee82524fe5417a1319986f0097ad0
Sean Albito <sean.albito@workos.com>
2024-08-21 15:15:11 -0700

    Add directory metadata

--- tests/utils/fixtures/mock_directory.py
+++ tests/utils/fixtures/mock_directory.py
@@ -1,10 +1,24 @@
 import datetime
 
-from workos.types.directory_sync import Directory
+from workos.types.directory_sync import (
+    Directory,
+    DirectoryMetadata,
+    DirectoryUsersMetadata,
+)
+
+
+class MockDirectoryUsersMetadata(DirectoryUsersMetadata):
+    def __init__(self, active=0, inactive=0):
+        super().__init__(active=active, inactive=inactive)
+
+
+class MockDirectoryMetadata(DirectoryMetadata):
+    def __init__(self, users, groups=0):
+        super().__init__(users=users, groups=groups)
 
 
 class MockDirectory(Directory):
-    def __init__(self, id):
+    def __init__(self, id, metadata=None):
         now = datetime.datetime.now().isoformat()
         super().__init__(
             object="directory",
@@ -15,6 +29,7 @@ class MockDirectory(Directory):
             name="Some fake name",
             state="active",
             type="gsuite directory",
+            metadata=metadata,
             created_at=now,
             updated_at=now,
         )

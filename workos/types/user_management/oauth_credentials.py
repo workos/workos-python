29e4df22e470c408c5cbbe69cff41322e81f8824
Sean Albito <sean.albito@workos.com>
2024-11-04 11:25:35 -0800

    Add OauthCredentialProviders

--- workos/types/user_management/oauth_credentials.py
+++ workos/types/user_management/oauth_credentials.py
@@ -1,11 +1,18 @@
-from typing import List
+from typing import Literal, List
 from workos.types.workos_model import WorkOSModel
+
+OauthCredentialProviders = Literal[
+    "AppleOauth",
+    "GitHubOauth",
+    "GoogleOauth",
+    "MicrosoftOauth",
+]
 
 
 class OauthCredentials(WorkOSModel):
     """Representation of a Oauth credentials"""
 
-    provider: str
+    provider: OauthCredentialProviders
     access_token: str
     refresh_token: str
     expires_at: int

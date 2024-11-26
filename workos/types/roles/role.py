946786c210c4ba68a67f03e5510ce4dee466bc4f
mattgd <matt.dzwonczyk@workos.com>
2024-11-26 10:00:14 -0500

    Add GET /organization/:orgId/roles support.

--- workos/types/roles/role.py
+++ workos/types/roles/role.py
@@ -1,8 +1,28 @@
 from typing import Literal, Optional, Sequence
 from workos.types.workos_model import WorkOSModel
 
+RoleType = Literal["EnvironmentRole", "OrganizationRole"]
 
-class Role(WorkOSModel):
+
+class RoleCommon(WorkOSModel):
     object: Literal["role"]
     slug: str
+
+
+# TODO: This is used for events/webhooks only. Rename to EventRole or something similar.
+class Role(RoleCommon):
     permissions: Optional[Sequence[str]] = None
+
+
+class OrganizationRole(RoleCommon):
+    id: str
+    name: str
+    description: Optional[str] = None
+    type: RoleType
+    created_at: str
+    updated_at: str
+
+
+class RolesList(WorkOSModel):
+    object: Literal["list"]
+    data: Sequence[OrganizationRole]

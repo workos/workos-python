6b6c66af41859b5d72107cb27b727dd9ba77fa48
Paul Asjes <p.asjes@gmail.com>
2024-12-02 15:35:06 +0100

    Use sequence instead of list

--- workos/types/user_management/session.py
+++ workos/types/user_management/session.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, TypedDict, Union
+from typing import Optional, Sequence, TypedDict, Union
 from enum import Enum
 from typing_extensions import Literal
 from workos.types.user_management.impersonator import Impersonator
@@ -17,10 +17,10 @@ class AuthenticateWithSessionCookieSuccessResponse(WorkOSModel):
     session_id: str
     organization_id: Optional[str] = None
     role: Optional[str] = None
-    permissions: Optional[List[str]] = None
+    permissions: Optional[Sequence[str]] = None
     user: User
     impersonator: Optional[Impersonator] = None
-    entitlements: Optional[List[str]] = None
+    entitlements: Optional[Sequence[str]] = None
 
 
 class AuthenticateWithSessionCookieErrorResponse(WorkOSModel):

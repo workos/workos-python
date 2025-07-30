56b0249ed24ebde0cfde2f26e6cc53701b31ce9b
Adam Jenkins <adam.jenkins@workos.com>
2025-07-30 08:10:11 -0300

    Expose SDK method to delete an organization domain

--- workos/organizations.py
+++ workos/organizations.py
@@ -128,6 +128,19 @@ class OrganizationsModule(Protocol):
         """
         ...
 
+    def delete_organization_domain(
+        self, organization_domain_id: str
+    ) -> SyncOrAsync[None]:
+        """Deletes a single Organization Domain
+
+        Args:
+            organization_domain_id (str): Organization Domain unique identifier
+
+        Returns:
+            None
+        """
+        ...
+
 
 class Organizations(OrganizationsModule):
     _http_client: SyncHTTPClient
@@ -236,6 +249,12 @@ class Organizations(OrganizationsModule):
     def delete_organization(self, organization_id: str) -> None:
         self._http_client.request(
             f"organizations/{organization_id}",
+            method=REQUEST_METHOD_DELETE,
+        )
+
+    def delete_organization_domain(self, organization_domain_id: str) -> None:
+        self._http_client.request(
+            f"organization_domains/{organization_domain_id}",
             method=REQUEST_METHOD_DELETE,
         )
 
@@ -355,6 +374,12 @@ class AsyncOrganizations(OrganizationsModule):
     async def delete_organization(self, organization_id: str) -> None:
         await self._http_client.request(
             f"organizations/{organization_id}",
+            method=REQUEST_METHOD_DELETE,
+        )
+
+    async def delete_organization_domain(self, organization_domain_id: str) -> None:
+        await self._http_client.request(
+            f"organization_domains/{organization_domain_id}",
             method=REQUEST_METHOD_DELETE,
         )
 

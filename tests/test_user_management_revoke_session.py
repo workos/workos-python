48460cde2db722f5eaca8ad42cedba529915a600
Adam Wolfman <adam@workos.com>
2025-10-15 11:23:52 -0600

    Remove unused mock session from test

--- tests/test_user_management_revoke_session.py
+++ tests/test_user_management_revoke_session.py
@@ -6,24 +6,6 @@ from tests.utils.syncify import syncify
 from workos.user_management import AsyncUserManagement, UserManagement
 
 
-def _mock_session(id: str):
-    now = "2025-07-23T14:00:00.000Z"
-    return {
-        "object": "session",
-        "id": id,
-        "user_id": "user_123",
-        "organization_id": "org_123",
-        "status": "revoked",
-        "auth_method": "password",
-        "ip_address": "192.168.1.1",
-        "user_agent": "Mozilla/5.0",
-        "expires_at": "2025-07-23T15:00:00.000Z",
-        "ended_at": now,
-        "created_at": now,
-        "updated_at": now,
-    }
-
-
 @pytest.mark.sync_and_async(UserManagement, AsyncUserManagement)
 class TestUserManagementRevokeSession:
     @pytest.fixture(autouse=True)

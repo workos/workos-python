b445127cc62589a21888bab5ad50dbd4a0b3e8d2
d.saner@enlyze.com <d.saner@enlyze.com>
2025-11-13 09:38:00 +0100

    adapt tests

--- tests/test_api_keys.py
+++ tests/test_api_keys.py
@@ -1,34 +1,40 @@
+# type: ignore
 import pytest
 
 from tests.utils.fixtures.mock_api_key import MockApiKey
 from tests.utils.syncify import syncify
-from workos.api_key import ApiKey, AsyncApiKey
+from workos.api_keys import API_KEY_VALIDATION_PATH, ApiKeys, AsyncApiKeys
 from workos.exceptions import AuthenticationException
 
 
-@pytest.mark.sync_and_async(ApiKey, AsyncApiKey)
-class TestApiKey:
+@pytest.mark.sync_and_async(ApiKeys, AsyncApiKeys)
+class TestApiKeys:
     @pytest.fixture
-    def mock_api_key_details(self):
-        api_key_details = MockApiKey()
-        return api_key_details.model_dump()
+    def mock_api_key(self):
+        return MockApiKey().dict()
+
+    @pytest.fixture
+    def api_key(self):
+        return "sk_my_api_key"
 
     def test_validate_api_key_with_valid_key(
         self,
         module_instance,
-        mock_api_key_details,
+        api_key,
+        mock_api_key,
         capture_and_mock_http_client_request,
     ):
+        response_body = {"api_key": mock_api_key}
         request_kwargs = capture_and_mock_http_client_request(
-            module_instance._http_client, mock_api_key_details, 200
+            module_instance._http_client, response_body, 200
         )
 
-        api_key_details = syncify(module_instance.validate_api_key())
+        api_key_details = syncify(module_instance.validate_api_key(value=api_key))
 
-        assert request_kwargs["url"].endswith("/api_keys/validate")
+        assert request_kwargs["url"].endswith(API_KEY_VALIDATION_PATH)
         assert request_kwargs["method"] == "post"
-        assert api_key_details.id == mock_api_key_details["id"]
-        assert api_key_details.name == mock_api_key_details["name"]
+        assert api_key_details.id == mock_api_key["id"]
+        assert api_key_details.name == mock_api_key["name"]
         assert api_key_details.object == "api_key"
 
     def test_validate_api_key_with_invalid_key(
@@ -43,4 +49,4 @@ class TestApiKey:
         )
 
         with pytest.raises(AuthenticationException):
-            syncify(module_instance.validate_api_key())
+            syncify(module_instance.validate_api_key(value="invalid-key"))

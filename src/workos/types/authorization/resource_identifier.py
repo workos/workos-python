d66d6ec51f5cca771ef249d23597da95e802fbbb
swaroopakkineni <swaroop@workos.com>
2026-02-23 05:59:46 -1000

    minor nit

--- src/workos/types/authorization/resource_identifier.py
+++ src/workos/types/authorization/resource_identifier.py
@@ -4,14 +4,10 @@ from typing_extensions import TypedDict
 
 
 class ParentResourceIdentifierById(TypedDict):
-    """Identify a parent resource by its WorkOS-assigned ID."""
-
     parent_resource_id: str
 
 
 class ParentResourceIdentifierByExternalId(TypedDict):
-    """Identify a parent resource by its external ID and resource type."""
-
     parent_resource_type_slug: str
     parent_resource_external_id: str
 
